383bb69ad90005834926d9d2c8b67ffd1ca38b26
Vladislav Klyukin <vk.azeria@gmail.com>
2023-07-05 21:16:04 +0500

    create .ts file

---
+++ src/scripts/dist/timer.js
@@ -0,0 +1,5 @@
+"use strict";
+const startButton = document.getElementById("start_button");
+const endButton = document.getElementById("end_button");
+const timer = document.getElementById("timer__counter");
+const endTimer = document.querySelector("#timer__end-time > span");
---
+++ src/scripts/timer.ts
@@ -0,0 +1,4 @@
+const startButton = document.getElementById("start_button")!;
+const endButton = document.getElementById("end_button")!;
+const timer = document.getElementById("timer__counter")!;
+const endTimer = document.querySelector("#timer__end-time > span")!;
